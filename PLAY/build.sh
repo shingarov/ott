a90fe9ca47436e344c0021e160f4ebe7656ab091
Boris Shingarov <shingarov@labware.com>
2024-03-15 23:39:50 -0400

    Add PLAY/Makefile

--- PLAY/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-../bin/ott -show_sort true -quotient_rules false -i simple.ott -o simple_parser.mly -o simple_lexer.mll -o simple_ast.ml
-
-ocamlbuild -use-ocamlfind -use-menhir -menhir "menhir --infer" -package pprint main.byte
-
